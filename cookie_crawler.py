3381f8d633ca4f366f12fe5e8f8fa575c4a4ab2e
Ranjith V <166651327+net-cipher@users.noreply.github.com>
2025-02-12 23:05:43 +0530

    Update cookie_crawler.py

--- cookie_crawler.py
+++ cookie_crawler.py
@@ -6,6 +6,6 @@ for i in range(25):
 	cookie = 'name={}'.format(i)
 	headers = {'Cookie':cookie}
 
-	r = requests.get('http://mercury.picoctf.net:27177/check',headers=headers)
+	r = requests.get('                    ',headers=headers)      #give your url link in the empty space inside the requests.txt
 	if(r.status_code == 200) and ('picoCTF' in r.text):
 		print("You fond th flag" , r.text)
